21ddc5422a787c5fcd8d58e0fe1657f0f5f4221b
Subhan Asif <asifsubhan904@gmail.com>
2024-01-23 00:10:39 +0500

    made changes in settings.py

--- core/settings.py
+++ core/settings.py
@@ -119,8 +119,7 @@ USE_TZ = True
 
 import os
 STATIC_URL = '/static/' 
-STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
-
+STATIC_ROOT = 'staticfiles'
 STATICFILES_DIR = {
     os.path.join(BASE_DIR , "public/static")
 }
